2956b5ab743ba5574da40c4f0ac64e214ccd7e3b
serverigit <serverigit@proton.me>
2024-09-18 20:08:43 +0300

    Add buttons to resize and clear the canvas

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const gridContainer = document.querySelector("#grid-container");
+const sizeBtn = document.querySelector("#size-button");
+const clearBtn = document.querySelector("#clear-button");
 
 const grid = document.createElement("div");
 grid.setAttribute("class", "grid");
 
-let number = 16;
+let number;
 
-grid.style.width = 100 / number + "%";
-grid.style.height = 100 / number + "%";
+sizeBtn.addEventListener("click", function (number) {
+  const gridClone = document.querySelectorAll(".grid");
+  gridClone.forEach((element) => {
+    element.remove();
+  });
+  number = prompt("Set your size. Number must be between 1-100.");
+  grid.style.width = 100 / number + "%";
+  grid.style.height = 100 / number + "%";
+  for (let i = 1; i <= number * number; i++) {
+    gridContainer.append(grid.cloneNode());
+  }
+});
 
-for (let i = 1; i <= number * number; i++) {
-  gridContainer.append(grid.cloneNode());
-}
+clearBtn.addEventListener("click", () => {
+  const gridClone = document.querySelectorAll(".grid");
+  gridClone.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
+});
 
 gridContainer.addEventListener("mouseover", function (e) {
   if (e.target.matches(".grid")) {
