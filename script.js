74f203e90fafad517d41e12c2cd5f6b45ff89cb3
serverigit <serverigit@proton.me>
2024-09-19 10:53:56 +0300

    Add isNaN() valuation to check user's input

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function clearGrid() {
 
 sizeBtn.addEventListener("click", function () {
   number = parseInt(prompt("Set your size. Number must be between 1-100."));
-  if (number > 100 || number <= 0) {
+  if (number > 100 || number <= 0 || isNaN(number)) {
     alert("Invalid number. Number must be between 1-100.");
     return;
   }
