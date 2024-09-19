f39cfadb3cc0d00e8414f8d8f51f2bda328530fa
serverigit <serverigit@proton.me>
2024-09-19 16:13:16 +0300

    Add buttons to switch between black pen and eraser

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const gridContainer = document.querySelector("#grid-container");
 const sizeBtn = document.querySelector("#size-button");
 const clearBtn = document.querySelector("#clear-button");
 const informationDiv = document.querySelector("#information");
-
+const blackBtn = document.querySelector("#black-button");
+const eraserBtn = document.querySelector("#eraser-button");
+const rainbowBtn = document.querySelector("#rainbow-button");
 const grid = document.createElement("div");
-grid.setAttribute("class", "grid");
 
+grid.setAttribute("class", "grid");
 let number = 16;
 
 function generateGrid(number) {
@@ -41,11 +43,51 @@ sizeBtn.addEventListener("click", function () {
 
 clearBtn.addEventListener("click", clearGrid);
 
+let black = true;
+let eraser = false;
+let rainbow = false;
+
+blackBtn.addEventListener("click", () => {
+  black = true;
+  eraser = false;
+  rainbow = false;
+});
+
+eraserBtn.addEventListener("click", () => {
+  eraser = true;
+  black = false;
+  rainbow = false;
+})
+
+rainbowBtn.addEventListener("click", () => {
+  rainbow = true;
+  eraser = false;
+  black = false;
+})
+
 gridContainer.addEventListener("mouseover", (e) => {
   if (e.target.matches(".grid")) {
-    e.target.style.backgroundColor = "black";
+    if (black) {
+      e.target.style.backgroundColor = "black";
+    }
+    if (eraser) {
+      e.target.style.backgroundColor = "white";
+    }
+    if (rainbow) {
+      e.target.style.backgroundColor = "black";
+    }
   }
 });
 
 // Add rainbow effect
+  // red, orange, yellow, green, blue, indigo, and violet
+
+function getRandomColor() {
+  const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+  let randomNumber = Math.floor(Math.random() * rainbowColors.length);
+  let randomColor = rainbowColors[randomNumber];
+  console.log(randomColor);
+  return randomColor;
+}
+
 // Add darkening effect
